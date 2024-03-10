da4068dcc99f3b17e53ea0970ec6145487de8e7b
Will Walton <will@endless3dge.com>
2024-03-10 15:49:18 -0500

    Update frontendscript.js
    began footer script

--- frontendscript.js
+++ frontendscript.js
@@ -19,3 +19,19 @@ const toggleBtnBackToTop = () => {
 btnBackToTop.addEventListener('click', backToTop);
 window.addEventListener('scroll', toggleBtnBackToTop);
 
+// Footer
+class Footer extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <div class="section">
+        <div class="container-fluid">
+            <div class="footer">
+                <div class="sort-note">
+                    <a class="navbar-brand" href="webdev.html">Will Walton</a>
+                    <p>I'm Will Walton, a junior Web Designer with front-end development skills. I'm enthusiastic about using my varied experiences to solve complex problems and craft enjoyable user experiences. 
+                    My expertise lies in web development, front-end web development, and UI design.</p>
+                    <hr style="margin: 20px 0; color: rgb(82, 82, 82); width: 60%;">
+                    <span class="contact"><a href="mailto:waltonjrwillie@gmail.com" class="email">
+                        `
+    }
+}
